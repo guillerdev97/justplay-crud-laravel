0a6aea9db19b65c60709a3e9ebdead19a07ad207
Guillermo García <guillermieres97@gmail.com>
2022-08-25 10:01:49 +0200

    styles on create view

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,52 +3,52 @@
 @section('content')
 
 <section>
-<form action="{{ route('storeEvent') }}" method="post">
-    @csrf
-    <div class="left">
-        <div>
-            <label for="title">Event name</label><br>
-            <input type="text" name="title" id="title">
-        </div>
-
-        <div>
-            <label for="event_date">Event date</label><br>
-            <input type="text" name="event_date" id="event_date">
-        </div>
-
-        <div>
-            <label for="description">Event description</label><br>
-            <textarea name="description" id="description" class="textarea"></textarea>
-        </div>
-    </div>
-
-    <div class="right">
-        <div>
-            <label for="">Insert image 📷</label><br>
-            <input type="text" name="img" id="img">
-        </div>
-
-        <img src="https://mejorconsalud.as.com/wp-content/uploads/2018/06/saludo-sol-yoga.jpg" alt="">
-
-        <div>
-            <label for="">Spaces</label>
-            <input type="text" name="spaces" class="spaces">
-        </div>
-
-        <div class="buttons">
+    <form action="{{ route('storeEvent') }}" method="post">
+        @csrf
+        <div class="left">
             <div>
-                <a href="{{ route('home') }}">Cancel</a>
+                <label for="title">Event name</label><br>
+                <input type="text" name="title" id="title">
             </div>
 
             <div>
-                <input type="reset" value="Reset">
+                <label for="event_date">Event date</label><br>
+                <input type="text" name="event_date" id="event_date">
             </div>
 
             <div>
-                <button type="submit">Create</button>
+                <label for="description">Event description</label><br>
+                <textarea name="description" id="description" class="textarea"></textarea>
             </div>
         </div>
-</form>
+
+        <div class="right">
+            <div class="imageBox">
+                <label for="">Insert image 📷</label><br>
+                <input type="text" name="img" id="img">
+            </div>
+
+            <img src="https://mejorconsalud.as.com/wp-content/uploads/2018/06/saludo-sol-yoga.jpg" alt="">
+
+            <div class="spaces">
+                <label for="">Spaces</label>
+                <input type="text" name="spaces">
+            </div>
+
+            <div class="buttons">
+                <div>
+                    <a href="{{ route('home') }}"><button class="btn btn-danger fs-4">Cancel</button></a>
+                </div>
+
+                <div>
+                    <button type="reset" class="btn btn-secondary fs-4">Reset</button>
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-success fs-4">Create</button>
+                </div>
+            </div>
+    </form>
 </section>
 
 @endsection
