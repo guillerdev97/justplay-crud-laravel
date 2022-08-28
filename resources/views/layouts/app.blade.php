a99474f5ff46ed0ed166d106b2e57d09000c31a7
Guillermo García <guillermieres97@gmail.com>
2022-08-28 17:27:55 +0200

    style: create and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,14 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  
+    <script src="https://kit.fontawesome.com/1f23ecb853.js" crossorigin="anonymous"></script>
     <!-- Scripts -->
    
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- CSS only -->
    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js', ])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', ])
    
 </head>
 <body>
