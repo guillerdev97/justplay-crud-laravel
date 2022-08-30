6eaea224f5b37b75e7c4ab765c61d9aacd411113
Guillermo García <guillermieres97@gmail.com>
2022-08-30 21:48:08 +0200

    my events view made by susi

---
+++ resources/myEvents.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('title', 'MY EVENTS')
+
+@section('content')
+
+  <div class="my_events_page inline-flex mx-2 m-10 ">
+    <div class="My Events  ml-0 mr-20 p-3 hover:font-bold">
+      <button type="button">My Events</button>
+    </div>
+
+    <div class="Past Events ml-20 p-3 hover:font-bold">
+      <button type="button">Past Events</button>
+    </div>
+  </div>
+
+  <a href="#" class="flex flex-col items-center bg-white  md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-screen mx-10">
+    <img class="object-cover w-full h-200 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_1280.jpg" alt="Event Image">
+
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <div class="flex justify-between items-center w-screen bg-green-200 p-2 title">
+        <h5 class="w-full text-2xl font-bold tracking-tight text-gray-900 dark:text-white uppercase ">Event Title</h5>
+        <div class="p-2 bg-green-200 ">
+          <button type="submit" class=" p-1 border-4 font-bold border-white text-red-600" id="unsubscribebutton">UNSUBSCRIBE</button>
+        </div>
+      </div>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 bg-gray-200 p-2 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.jlkjlkjljklkl hjjlkjkl skskssj sksksksk skHere are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. </p>
+    </div>
+
+    <div class="flex flex-center px-10 spaces  ">
+      <p>Places</p>
+      <p class="border border-black rounded-xl bg-gray-200 px-4 my-3 ">15</p>
+    </div>
+
+  </a>
+  <a href="#" class="flex flex-col items-center bg-white  md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-screen mx-10">
+    <img class="object-cover w-full h-200 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_1280.jpg" alt="Event Image">
+
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <div class="flex justify-between items-center w-screen bg-green-200 p-2 title">
+        <h5 class="w-full text-2xl font-bold tracking-tight text-gray-900 dark:text-white uppercase ">Event Title</h5>
+        <div class="p-2 bg-green-200 ">
+          <button type="submit" class=" p-1 border-4 font-bold border-white text-red-600" id="unsubscribebutton">UNSUBSCRIBE</button>
+        </div>
+      </div>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 bg-gray-200 p-2 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.jlkjlkjljklkl hjjlkjkl skskssj sksksksk skHere are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. </p>
+    </div>
+
+    <div class="flex flex-center px-10 spaces">
+      <p>Places</p>
+      <p class="border border-black rounded-xl bg-gray-200 px-4 my-3 ">15</p>
+    </div>
+
+  </a>
+
+  <a href="#" class="flex flex-col items-center bg-white  md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-screen mx-10">
+    <img class="object-cover w-full h-200 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_1280.jpg" alt="Event Image">
+
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <div class="flex justify-between items-center w-screen bg-green-200 p-2 title">
+        <h5 class="w-full text-2xl font-bold tracking-tight text-gray-900 dark:text-white uppercase ">Event Title</h5>
+        <div class="p-2 bg-green-200 ">
+          <button type="submit" class=" p-1 border-4 font-bold border-white text-red-600" id="unsubscribebutton">UNSUBSCRIBE</button>
+        </div>
+      </div>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 bg-gray-200 p-2 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.jlkjlkjljklkl hjjlkjkl skskssj sksksksk skHere are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. </p>
+    </div>
+
+    <div class="flex flex-center px-10 spaces  ">
+      <p>Places</p>
+      <p class="border border-black rounded-xl bg-gray-200 px-4 my-3 ">15</p>
+    </div>
+
+  </a>
+
+@endsection
