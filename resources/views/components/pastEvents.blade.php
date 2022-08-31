46f84dde9029b5e8cb90e4b651b00c884fbc1676
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-08-31 23:32:59 +0200

    cambios estilos

--- resources/views/components/pastEvents.blade.php
+++ resources/views/components/pastEvents.blade.php
@@ -5,50 +5,23 @@
     <div class="relative"> 
       <a href="{{ route('showEvent', $event->id) }}">
       <button class="absolute p-1 w-full flex justify-end" type="button">
-        <img src="{{ asset('img/information.png') }}" alt="information" width="30" height="30">
+        <img src="{{ asset('img/information.png') }}" alt="information" class="w-9 h-auto items-center p-0">
       </button>
       </a>
       <a href="#">
-        <img class="rounded-t-lg" src="{{ $event->img }}" alt="" />
+        <img class="rounded-t-lg p-0" src="{{ $event->img }}" alt="" />
       </a>
     </div> 
     <div class="p-3">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{{ $event->title }}</h5>
+          <h2 class="mb-2 text-2xl tracking-tight text-gray-900 dark:text-black">{{ $event->title }}</h2>
         </a>
-        <p class="mb-3 font-semibold text-black-700 dark:text-black-400">{{ $event->event_date }}</p>
-        <p class="mb-3 font-semibold text-black-700 dark:text-black-400">{{ $event->spaces }} cupos</p>
+        <p class="mb-1  text-black-700 dark:text-black-400">{{ $event->event_date }}</p>
+        <p class="mb-1  text-black-700 dark:text-black-400">{{ $event->spaces }} cupos</p>
         <div class="d-flex">
-          <a href="#" class="me-auto items-center py-2 px-3 ml-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href="#" class="me-auto items-center py-2 px-3 ml-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-red-800">
               Subscribe
-          </a>
-          
-         <!--  @if(Auth::check() && Auth::user()->isAdmin)
-          <a href="#">
-          <button type="button" class="focus:outline-none  bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2 mr-1 dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-green-800">
-            <img src="{{ asset('img/favorite.png') }}" alt="edit event" width="16" height="16">
-          </button>
-          </a>
-          @endif
-
-          @if(Auth::check() && Auth::user()->isAdmin)
-          <a href="{{ route('editEvent', ['id' => $event->id])}}">
-          <button type="button" class="focus:outline-none  bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2 mr-1 dark:bg-green-800 dark:hover:bg-blue-700 dark:focus:ring-green-800">
-            <img src="{{ asset('img/edit.png') }}" alt="edit event" width="16" height="16">
-          </button>
-          </a>
-          @endif
-
-          @if(Auth::check() && Auth::user()->isAdmin)
-          <form action="{{ route('delete', ['id' => $event->id]) }}" method="POST">
-            @method('delete')
-            @csrf
-            <button type="submit" class="focus:outline-none  bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg  px-2 py-2 mr-1  dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-red-900" onclick="return confirm('Are you sure you want to delete this event? {{ $event->title }}')">
-              <img src="{{ asset('img/delete.png') }}" alt="delete event" width="16" height="16">
-            </button>
-          </form>
-          @endif -->
-          
+          </a> 
         </div>
     </div>
 </div>
--- resources/views/pastEvents.blade.php
+++ resources/views/pastEvents.blade.php
@@ -1,11 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="m-10 grid justify-items-center text-3xl">
+  <h2 class="text-center ">WHAT IS COMMING!</h2>
+  <div class="bg-red-600 h-1 w-20"></div>
+</section>
 
-<!-- <h2 class="text-center">Listado de Eventos</h2> -->
 
-<a href="{{ route('home') }}">Incoming Events</a>
-<p>Past Events</p>
+
+@if(Auth::check() && Auth::user()->isAdmin)
+<a href="{{ route('createEvent') }}" class="flex item-center m-4 ">
+  <button type="button" class="focus:outline-none  bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2 mr-1 dark:bg-green-800 dark:hover:bg-blue-700 dark:focus:ring-green-800">
+    <img src="{{ asset('img/add.png') }}" alt="edit event" class="w-5 h-auto p-0">
+  </button>
+  <p class="m-2 text-lg">Add New Event</p>
+</a>
+@endif
+
+<section class="m-6 grid justify-items-center text-2xl">
+  <h2 class="text-center ">EVENT LIST</h2>
+  <div class="bg-green-600 h-1 w-20"></div>
+</section>
+<section class="flex m-4">
+  <a href="{{ route('home') }}">
+    <h2 class="m-2 text-2xl">INCOMING EVENTS</h2>
+  </a>
+  <a href="{{ route('pastEvents') }}">
+    <h2 class="m-2 text-2xl underline">PAST EVENTS</h2>
+  </a>
+</section>
 
 <x-pastEvents :events="$events"></x-pastEvents>
 
