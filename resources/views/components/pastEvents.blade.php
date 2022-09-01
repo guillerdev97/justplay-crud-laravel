99241d499c05ebd4de174c5224d968ccce6b0411
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-01 09:26:06 +0200

    cambios estilos

--- resources/views/components/pastEvents.blade.php
+++ resources/views/components/pastEvents.blade.php
@@ -19,9 +19,7 @@
         <p class="mb-1  text-black-700 dark:text-black-400">{{ $event->event_date }}</p>
         <p class="mb-1  text-black-700 dark:text-black-400">{{ $event->spaces }} cupos</p>
         <div class="d-flex">
-          <a href="#" class="me-auto items-center py-2 px-3 ml-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-red-800">
-              Subscribe
-          </a> 
+           
         </div>
     </div>
 </div>
