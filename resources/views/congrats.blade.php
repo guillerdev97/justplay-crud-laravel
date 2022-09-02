e17bee5a3f5f41e6b8b15843fbb924906c072757
Guillermo García <guillermieres97@gmail.com>
2022-09-02 15:09:31 +0200

    fix bug

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
+<?php
+header('refresh:2;url=/');
+?>
 <h2 class="congrats-message">
     Congrats! You are subscribed to {{ $event->title }} event!
 </h2>
