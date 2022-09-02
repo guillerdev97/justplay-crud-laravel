bfcebf98c42217ead7ce8c426f8bfc12f9ff85c6
Susana M.A <asturias.susi.pro@gmail.com>
2022-09-02 09:57:25 +0200

    Correction Funtionality My Events

--- resources/views/myEvents.blade.php
+++ resources/views/myEvents.blade.php
@@ -16,13 +16,17 @@
 
 @foreach ($eventsOfUser as $eventOfUser)
 @if ($eventOfUser->event_date > now())
-<a href="{{ route('showEvent', $eventOfUser->id) }}" class="flex flex-col items-center mx-20 md:flex-row md:max-w-sm ">
+<section class="flex flex-col items-center mx-20 md:flex-row md:max-w-sm ">
   <img class="object-cover w-full h-200 rounded-t-sm ms:h-auto sm:w-48 sm:rounded-none md:rounded-none sm" src="{{ $eventOfUser->img }}" alt="Event Image">
 
   <div id="paragraph" class="flex flex-col justify-between p-4 leading-normal max-w-screen-md">
     <div class="max-w-screen-sm flex justify-between items-center w-screen bg-green-200 p-2 mt-4 title">
-      <h5 class="w-full text-xl font-bold tracking-tight text-gray-900 dark:text-black uppercase">{{$eventOfUser->title}}</h5>
-      <button type="submit" class="w-1/3 text-xs p-1 border-4 font-bold border-white text-red-600" id="unsubscribebutton">UNSUBSCRIBE</button>
+      <a href="{{ route('showEvent', $eventOfUser->id) }}">
+        <h5 class="w-full text-xl font-bold tracking-tight text-gray-900 dark:text-black uppercase">{{$eventOfUser->title}}</h5>
+      </a>
+      <a href="{{ route('cancelInscription', $eventOfUser->id) }}">
+        <button type="submit" class="text-xs p-1 border-4 font-bold border-white text-red-600" id="unsubscribebutton">UNSUBSCRIBE</button>
+      </a>
     </div>
     <p class="mb-3 font-normal text-gray-700  bg-gray-200 p-2 ">{{ $eventOfUser->description }} </p>
   </div>
